eb489175d6a35902316d4fcfa9c9c4c7f0783204
Amy <amyemoretti@gmail.com>
2018-10-05 20:09:32 -0500

    updating

--- clickygame/src/components/Catcard/Catcard.jsx
+++ clickygame/src/components/Catcard/Catcard.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Catcard.css";
 
-const CatCard = props => (
-        <div className="card">
-            <div className="img-container">
-                <a onClick={() => props.selectCat(props.name)} className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}>
-                    <img alt={props.name} src={props.image} />
-                </a>
-            </div>
-        </div>
-    );
+const Catcard = props => (
+    <div className="card" onClick={props.imageClick}>
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
 
 export default Catcard;
