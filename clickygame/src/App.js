d06a29f432c0527de37b48c509e2026607cf3be2
Amy <amyemoretti@gmail.com>
2018-10-05 19:53:52 -0500

    component work

---
+++ clickygame/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import cats from "./cats.json";
+import Navbar from "./components/Navbar";
+import Instructions from "./components/Instructions";
+import Catcard from "./components/Catcard";
+import Footer from "./components/Footer";
+
+class App extends Component {
+
+}
+
+export default App;
--- clickygame/src/components/Catcard/Catcard.jsx
+++ clickygame/src/components/Catcard/Catcard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./Catcard.css";
 
 const CatCard = props => (
-
-);
+        <div className="card">
+            <div className="img-container">
+                <a onClick={() => props.selectCat(props.name)} className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}>
+                    <img alt={props.name} src={props.image} />
+                </a>
+            </div>
+        </div>
+    );
 
 export default Catcard;
