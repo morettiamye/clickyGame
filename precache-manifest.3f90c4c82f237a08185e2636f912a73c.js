82d8857249de0e66b7bff52e011a4a10e0418afe
Amy <amyemoretti@gmail.com>
2018-10-05 21:15:03 -0500

    Updates

---
+++ precache-manifest.3f90c4c82f237a08185e2636f912a73c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "c676c726de371e5098ef",
+    "url": "/clickyGame/static/js/main.c676c726.chunk.js"
+  },
+  {
+    "revision": "27625f046522a1701404",
+    "url": "/clickyGame/static/js/1.27625f04.chunk.js"
+  },
+  {
+    "revision": "c676c726de371e5098ef",
+    "url": "/clickyGame/static/css/main.046ad402.chunk.css"
+  },
+  {
+    "revision": "e5aa4670cf4c215c8cf2b392ea96f40f",
+    "url": "/clickyGame/index.html"
+  }
+];
